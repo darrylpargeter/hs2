e74984696f7dca9f21e3a0a52d14d2bd8ca1bbd6
darrylpargeter <darrylpargeter@gmail.com>
2017-12-09 10:08:47 +0000

    working

--- src/App.js
+++ src/App.js
@@ -42,7 +42,14 @@ function notifyMe(res) {
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    var notification = new Notification(res);
+    // var notification = new Notification(res);
+    try {
+      new Notification(res);
+    } catch (e) {
+      if (e.name == "TypeError") {
+        ServiceWorkerRegistration.showNotification(res)
+      }
+    }
   }
 
   // Otherwise, we need to ask the user for permission
@@ -50,7 +57,7 @@ function notifyMe(res) {
     Notification.requestPermission(function (permission) {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        var notification = new Notification("Hi there!");
+        // var notification = new Notification("Hi there!");
       }
     });
   }
